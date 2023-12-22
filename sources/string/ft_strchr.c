08233aedb4bd0153fceb7dcb9b82748760748733
Jean-Baptiste Brousse <jbrousse@made-f0Ar11s12.clusters.42paris.fr>
2023-12-22 16:06:16 +0100

    ok

--- sources/string/ft_strchr.c
+++ sources/string/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:43:13 by jbrousse          #+#    #+#             */
-/*   Updated: 2023/11/08 17:03:09 by jbrousse         ###   ########.fr       */
+/*   Updated: 2023/12/22 16:05:16 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 	{
 		if (s[i] == (char)c)
 			return ((char *)s + i);
 		i++;
 	}
-	if (s[i] == c)
-		return ((char *)s + i);
 	return (0);
 }
