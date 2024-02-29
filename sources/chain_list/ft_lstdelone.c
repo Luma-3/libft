d87996d3bdd32033d902edbaf3730777929dbc00
Jean-Baptiste Brousse <jbrousse@paul-f5Br6s5.clusters.42paris.fr>
2024-02-29 16:32:22 +0100

    fix lstdelone if first elem

--- sources/chain_list/ft_lstdelone.c
+++ sources/chain_list/ft_lstdelone.c
@@ -6,16 +6,19 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:45:42 by jbrousse          #+#    #+#             */
-/*   Updated: 2024/02/29 16:18:26 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:30:57 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *prev, t_list *node, void (*del)(void *))
+void	ft_lstdelone(t_list **lst, t_list *prev,
+			t_list *node, void (*del)(void *))
 {
 	if (prev && node -> next)
 		prev -> next = node -> next;
+	else if (!prev && node -> next)
+		lst = &node -> next;
 	del(node -> content);
 	free(node);
 }
