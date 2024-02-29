0b86d0856ae233e6014db10b8932def689a7d23e
Jean-Baptiste Brousse <jbrousse@paul-f5Br6s5.clusters.42paris.fr>
2024-02-29 16:43:48 +0100

    fix again lstdelone

--- sources/chain_list/ft_lstdelone.c
+++ sources/chain_list/ft_lstdelone.c
@@ -6,20 +6,18 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:45:42 by jbrousse          #+#    #+#             */
-/*   Updated: 2024/02/29 16:35:17 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:42:28 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **lst, t_list *prev,
-			t_list *node, void (*del)(void *))
+void	ft_lstdelone(t_list **lst, void (*del)(void *))
 {
-	if (prev && node -> next)
-		prev -> next = node -> next;
-	else if (!prev && node -> next)
-		lst = &node -> next;
-	(void)lst;
+	t_list	*node;
+
+	node = *lst;
+	*lst = node -> next;
 	del(node -> content);
 	free(node);
 }
